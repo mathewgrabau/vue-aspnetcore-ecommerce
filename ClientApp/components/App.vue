d49592e80b8a3f1ee3a9f34474bcc7f9fe98020f
Mathew Grabau <mgrabau@mathewgrabau.ca>
2018-08-15 23:02:00 -0500

    Setup a product list component

--- ClientApp/components/App.vue
+++ ClientApp/components/App.vue
@@ -1,23 +1,17 @@
 <template>
 	<div>
-		<h1>Welcome to Hands on Vue.js with ASP.NET Core!</h1>
-		<p>
-			The time is: {{ time }}
-		</p>
-		<p>
-			The current users of our system are:
-			<ul>
-				<li v-for="user in users" :key="user.userName">
-					{{ user.fullName }} - {{ user.userName }}
-				</li>
-			</ul>
-		</p>
+		<product-list />
 	</div>
 </template>
 
 <script>
+import ProductList from './products/List.vue';
+
 export default {
 	name: 'app',
+	components: {
+		ProductList
+	},
 	data() {
 		return {
 			time: new Date().toString(),
