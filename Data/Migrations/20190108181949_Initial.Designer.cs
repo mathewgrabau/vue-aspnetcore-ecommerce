26f2f776702e70452113d6ceb78760b20ecfda6d
Mathew Grabau <mgrabau@mathewgrabau.ca>
2019-01-08 12:21:26 -0600

    Updated the initial migration (cover an SQLite incompat).

--- Data/Migrations/20190108181949_Initial.Designer.cs
+++ Data/Migrations/20190108181949_Initial.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using FirstProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FirstProject.Migrations
 {
     [DbContext(typeof(FirstProjectContext))]
-    partial class FirstProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20190108181949_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
